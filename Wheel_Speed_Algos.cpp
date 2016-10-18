306f0b22aca275ed232d57d9d67a460d0a8f428a
Kyle Dominguez <kyledominguez98@gmail.com>
2016-10-18 00:02:13 -0400

    Made more clear comments

--- Wheel_Speed_Algos.cpp
+++ Wheel_Speed_Algos.cpp
@@ -4,7 +4,6 @@
 
 const int STARBOARD_PIN = 2;
 const int PORT_PIN = 3;
-const int RPM_PERIOD = 20;
 
 volatile unsigned int sboard_curr = 0;
 volatile unsigned int port_curr = 0;
@@ -26,6 +25,12 @@ void port_click()
 //interrupt routine for timer2 overflow.
 //timer 2 is an 8bit timer with a 1024 scaling
 //Will run at 8MHz/256/1024 so ~30Hz
+//also am only pretty sure of 8MHz, if it's in fact 16 just some constant changing.
+
+//timer1 overflows with a freq. of 8MHz/65536/1024 so ~ 1.9 seconds
+//timer1 is at 125,000 Hz so one unit is 8*10^-6
+//one minute is 7,500,000 timer units
+#define MAGIC 7500000
 ISR(TIMER2_OVF_vect)
 {
 	//allow this interrupt to be interrupted
@@ -33,20 +38,21 @@ ISR(TIMER2_OVF_vect)
 	TCNT1 = 0x0000;
 	int sboard_delta = sboard_curr - sboard_old;
 	int port_delta = port_curr - port_old;
-	//Make sure that it's the difference between with ints and stuff
-	sboard_delta = sboard_delta < 0 ? sboard_delta : (sboard_curr - (sboard_old - INT_MAX));
+	//make sure that it's the absolute difference regardless of overflows
+	//assuming 16bit integers. I'm pretty sure that's what this is.
+	sboard_delta = sboard_delta < 0 ? sboard_delta : (sboard_curr - (sboard_old - 0xFFFF));
 	port_delta = port_delta < 0 ? port_delta : (port_curr - (port_old - 0xFFFF));
-	//timer1 overflows with a freq. of 8MHz/65536/1024 so ~ 1.9 seconds
-	//timer1 is at 125,000 Hz so one unit is 8*10^-6
-	//one minute is 7,500,000 timer units
-	int sboard_rpm = 7500000000 / sboard_delta / 22;
-	int port_rpm = 75000000 / port_delta / 22;
+	//converts clicks into RPM
+	//proper math is that one click per tick times MAGIC (7,500,000) / teeth (22) will net RPM.
+	int sboard_rpm = MAGIC / sboard_delta / 22;
+	int port_rpm = MAGIC / port_delta / 22;
 
 	Serial.print("port rpm:");
 	Serial.println(port_rpm);
 	Serial.print("starboard rpm:");
 	Serial.println(sboard_rpm);
 }
+#undef MAGIC
 
 void setup()
 {
@@ -64,14 +70,18 @@ void setup()
 
 	//enable the timer2 - 8bit timer
 	TIMSK = 1<<TOIE2;
-	TCNT2 = 0x00;
-	//prescalar of 1024
+	//set the count to 0
+	TCNT2 = 0;
+	//CA22 is timer 2 prescalar of 1024
+	//CS20 enables timer2
 	TCCR2 = (1<<CA22) | (1<<CS20);
 
 	//enable timer 1 - 16bit timer
 	TIMSK |= 1<<TOIE1;
-	TCNT1=0x0000;
-	//prescalar of 64
+	//set the count to 0
+	TCNT1=0;
+	//CS10 starts timer 1
+	//CS11 is timer 1 prescalar of 64
 	TCCR1 = (1<<CS10) | (1<<CS11);
 
 	//enable interrupts
