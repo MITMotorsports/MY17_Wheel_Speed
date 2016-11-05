00ee1ff00b48e80cfa58f22c26e6f607aac406f8
Kyle Dominguez <kyledominguez98@gmail.com>
2016-11-05 18:55:58 -0400

    Added documentation, made comments clear, and eliminated potential overflows

--- Wheel_Speed_Algos.cpp
+++ Wheel_Speed_Algos.cpp
@@ -5,10 +5,10 @@
 const int STARBOARD_PIN = 2;
 const int PORT_PIN = 3;
 
-volatile unsigned int sboard_curr = 0;
-volatile unsigned int port_curr = 0;
-volatile unsigned int sboard_old = 0;
-volatile unsigned int port_old = 0;
+volatile uint16_t sboard_curr = 0;
+volatile uint16_t port_curr = 0;
+volatile uint16_t sboard_old = 0;
+volatile uint16_t port_old = 0;
 
 void sboard_click()
 {
@@ -18,7 +18,7 @@ void sboard_click()
 
 void port_click()
 {
-	port_old = port_old;
+	port_old = port_curr;
 	port_curr= TCNT1;
 }
 
@@ -26,35 +26,42 @@ void port_click()
 //timer 2 is an 8bit timer with a 1024 scaling
 //Will run at 8MHz/256/1024 so ~30Hz
 //also am only pretty sure of 8MHz, if it's in fact 16 just some constant changing.
-
-//timer1 is at 125,000 Hz (8 MHz / 64) so one unit is 8*10^-6
-//one minute is 7,500,000 timer units
-//8 MHz / 64 * 60 = MAGIC because that's the number of timer clicks per minute
-#define MAGIC 7500000
+//TIMER2_OVF_vect is triggered every time timer2 overflows
 ISR(TIMER2_OVF_vect)
 {
+	//timer1 is at 125,000 Hz (8 MHz / 64) so one unit is 8*10^-6
+	//one minute is 7,500,000 timer units
+	//8 MHz / 64 * 60 = MAGIC because that's the number of timer clicks per minute
+	#define MAGIC 7500000
+	//22 teeth on the wheel, 22 clicks per rotation, 1 semirotation is 1/22 rotation
+	#define TEETH 22
+
+	uint16_t local_sboard_curr = sboard_curr;
+	uint16_t local_port_curr = port_curr;
 	//allow this interrupt to be interrupted
 	sei();
 	//make it rarer that it requres more logic to handle
-	TCNT1 = 0x0000;
+	TCNT1 = 0;
 	//using longs is safer because we need signed and the ability to interpret > 16 bits
-	long sboard_delta = sboard_curr - sboard_old;
-	long port_delta = port_curr - port_old;
+	int32_t sboard_delta = local_sboard_curr - local_sboard_old;
+	int32_t port_delta = local_port_curr - local_port_old;
 	//make sure that it's the absolute difference regardless of overflows
 	//assuming 16bit integers. I'm pretty sure that's what this is.
-	sboard_delta = sboard_delta > 0 ? sboard_delta : (sboard_curr - (sboard_old - UINT_MAX));
-	port_delta = port_delta > 0 ? port_delta : (port_curr - (port_old - UINT_MAX));
+	sboard_delta = sboard_delta >= 0 ? sboard_delta : (local_sboard_curr - (local_sboard_old - UINT_MAX));
+	port_delta = port_delta >= 0 ? port_delta : (local_port_curr - (local_port_old - UINT_MAX));
 	//converts clicks into RPM
 	//proper math is that one click per tick times MAGIC (7,500,000) / teeth (22) will net RPM.
-	unsigned int sboard_rpm = MAGIC / sboard_delta / 22;
-	unsigned int port_rpm = MAGIC / port_delta / 22;
+	uint32_t sboard_rpm = sboard_delta == 0 ? 0 : MAGIC / sboard_delta / TEETH;
+	uint32_t port_rpm = port_delta == 0 ? 0 : MAGIC / port_delta / TEETH;
 
 	Serial.print("port rpm:");
 	Serial.println(port_rpm);
 	Serial.print("starboard rpm:");
 	Serial.println(sboard_rpm);
+
+	#undef MAGIC
+	#undef TEETH
 }
-#undef MAGIC
 
 void setup()
 {
@@ -70,6 +77,8 @@ void setup()
 	attachInterrupt(digitalPinToInterrupt(STARBOARD_PIN), sboard_click, RISING);
 	attachInterrupt(digitalPinToInterrupt(PORT_PIN), port_click, RISING);
 
+	//Link to datasheet with most of the magic numbers on it
+	//http://www.atmel.com/Images/Atmel-42735-8-bit-AVR-Microcontroller-ATmega328-328P_datasheet.pdf
 	//enable the timer2 - 8bit timer
 	TIMSK = 1<<TOIE2;
 	//set the count to 0
